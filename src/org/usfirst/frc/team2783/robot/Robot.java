2479edaedde90fe370ab8b514f28293a82e7f0d3
Jake Powell <jakedp7@gmail.com>
2016-01-14 20:03:22 -0500

    Removed unnecessary example classes and fixed imports.

--- src/org/usfirst/frc/team2783/robot/Robot.java
+++ src/org/usfirst/frc/team2783/robot/Robot.java
@@ -1,13 +1,12 @@
 
 package org.usfirst.frc.team2783.robot;
 
+import org.usfirst.frc.team2783.robot.subsystems.DriveBase;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import org.usfirst.frc.team2783.robot.commands.ExampleCommand;
-import org.usfirst.frc.team2783.robot.subsystems.DriveBase;
-import org.usfirst.frc.team2783.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -21,7 +20,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends IterativeRobot {
 	
 	public static final DriveBase driveBase = new DriveBase();
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
 
     Command autonomousCommand;
@@ -34,8 +32,7 @@ public class Robot extends IterativeRobot {
     public void robotInit() {
 		oi = new OI();
         chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", new ExampleCommand());
-//        chooser.addObject("My Auto", new MyAutoCommand());
+//      chooser.addObject("My Auto", new MyAutoCommand());
         SmartDashboard.putData("Auto mode", chooser);
     }
 	
