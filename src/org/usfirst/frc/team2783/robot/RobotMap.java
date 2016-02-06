9743ef737c606e69662d027bbaeb5687021232a2
davidparks11 <davidparks11@gmail.com>
2016-02-06 11:49:28 -0500

    Unfinished code. Test for Daniel and Owen

--- src/org/usfirst/frc/team2783/robot/RobotMap.java
+++ src/org/usfirst/frc/team2783/robot/RobotMap.java
@@ -10,11 +10,11 @@ public class RobotMap {
 	final public static int XBOX_CONTROLLER_ID = 0;
 	final public static int SHOOTER_JOYSTICK_ID = 1;
 	
-	final public static int SHOOTER_HORIZONTAL_AXIS_MOTOR = 0; //default setting. Motor ID unknown. CHANGE ME
-	final public static int SHOOTER_VERTICAL_AXIS_MOTOR = 1; //default setting. Motor ID unknown. CHANGE ME
-	final public static int SHOOTER_WHEEL_MOTOR_ID = 4; //default setting. Motor ID unknown. CHANGE ME
-	final public static int FRONT_LEFT_MOTOR_ID = 12;
+	final public static int SHOOTER_HORIZONTAL_AXIS_MOTOR = 14; //default setting. Motor ID unknown. CHANGE ME
+	final public static int SHOOTER_VERTICAL_AXIS_MOTOR = 12; //default setting. Motor ID unknown. CHANGE ME
+	final public static int SHOOTER_WHEEL_MOTOR_ID = 2; //default setting. Motor ID unknown. CHANGE ME
+	final public static int FRONT_LEFT_MOTOR_ID = 19;
 	final public static int REAR_LEFT_MOTOR_ID = 13;
 	final public static int FRONT_RIGHT_MOTOR_ID = 3;
-	final public static int REAR_RIGHT_MOTOR_ID = 2;
+	final public static int REAR_RIGHT_MOTOR_ID = 123;
 }
--- src/org/usfirst/frc/team2783/robot/subsystems/ShooterBase.java
+++ src/org/usfirst/frc/team2783/robot/subsystems/ShooterBase.java
@@ -1,9 +1,13 @@
 package org.usfirst.frc.team2783.robot.subsystems;
 
 import org.usfirst.frc.team2783.robot.RobotMap;
-import org.usfirst.frc.team2783.robot.commands.ShootOnly;
+import org.usfirst.frc.team2783.robot.commands.AbsoluteMagenticEncoderTest;
 import org.usfirst.frc.team2783.robot.commands.ShootRotation;
 
+import edu.wpi.first.wpilibj.CANTalon.FeedbackDevice;
+import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.CANSpeedController.ControlMode;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -15,26 +19,30 @@ public class ShooterBase extends Subsystem {
 	public ShooterBase() {
 		super();
 		shooterWheel = new CANTalon(RobotMap.SHOOTER_WHEEL_MOTOR_ID);
-		shooterWheel.setControlMode(0); // value for quad encoder. Does not work(FeedbackDevice.AnalogEncoder)3 equals analog encoder
+		shooterWheel.changeControlMode(TalonControlMode.Speed); // value for quad encoder. Does not work(FeedbackDevice.AnalogEncoder)3 equals analog encoder
+		shooterWheel.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Absolute);
 		horizontalAxis = new CANTalon(RobotMap.SHOOTER_HORIZONTAL_AXIS_MOTOR);
-		horizontalAxis.setControlMode(0); // value for quad encoder. Does not work(FeedbackDevice.AnalogEncoder)3 equals analog encoder
-		verticalAxis = new CANTalon(RobotMap.SHOOTER_VERTICAL_AXIS_MOTOR);
-		verticalAxis.setControlMode(0); // value for quad encoder. Does not work(FeedbackDevice.AnalogEncoder)3 equals analog encoder
-		
+		horizontalAxis.changeControlMode(TalonControlMode.Position);
+		horizontalAxis.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		verticalAxis = new CANTalon(RobotMap.SHOOTER_VERTICAL_AXIS_MOTOR);		
+		verticalAxis.changeControlMode(TalonControlMode.Position);
+		verticalAxis.setFeedbackDevice(FeedbackDevice.QuadEncoder);
 	}
 
 	public void initDefaultCommand() {
-		setDefaultCommand(new ShootRotation());
+		setDefaultCommand(new AbsoluteMagenticEncoderTest());
 	}
 	
 	public void setWheelSpeed(double rpmOutput) {
 		shooterWheel.set(rpmOutput);
 	}
 	
-	public void setHorizontalAxis(double rpmOutput) {
-		horizontalAxis.set(rpmOutput);
+	public void setHorizontalAxis(double degrees) {
+		//Testing for correct input using Abs Mag Encoder
+		verticalAxis.setPosition(degrees);
+		//horizontalAxis.set(rpmOutput);
 	}
-	public void setVerticalAxis(double rpmOutput) {
-		verticalAxis.set(rpmOutput);
+	public void setVerticalAxis(double output) {
+		verticalAxis.set(output);
 	}
 }
