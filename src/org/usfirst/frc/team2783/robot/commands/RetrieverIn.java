b1420f5a626de850abb7f77b248c9ce7f9a581b8
Jake Powell <jakedp7@gmail.com>
2016-03-06 01:14:00 -0500

    Added moving average [untested]
    Added moving average class and used in retriever current reading

--- src/org/usfirst/frc/team2783/robot/commands/RetrieverIn.java
+++ src/org/usfirst/frc/team2783/robot/commands/RetrieverIn.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team2783.robot.commands;
 
 import org.usfirst.frc.team2783.robot.Robot;
+import org.usfirst.frc.team2783.util.MovingAverage;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -8,9 +9,13 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class RetrieverIn extends Command {
+	
+	MovingAverage retrieverIntakeCurrent;
 
     public RetrieverIn() {
     	requires(Robot.retriever);
+    	
+    	retrieverIntakeCurrent = new MovingAverage(4);
     }
 
     // Called just before this Command runs the first time
@@ -20,7 +25,10 @@ public class RetrieverIn extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.retriever.setRetrieverVbus(0.75, true);
-    	System.out.println("3: " + Robot.PDBoard.getCurrent(3) + ", 11: " + Robot.PDBoard.getCurrent(11));
+    	
+    	Double currentCurrent = (Robot.PDBoard.getCurrent(3) + Robot.PDBoard.getCurrent(11)) / 2;
+    	Double movingAverage = retrieverIntakeCurrent.addValue(currentCurrent);
+    	System.out.println("Retriever Current: " + movingAverage);
     }
 
     // Make this return true when this Command no longer needs to run execute()
