0aed192e4167e93916fdabdeaa5a855ffee4fde3
Jake Powell <jakedp7@gmail.com>
2016-03-10 12:16:44 -0500

    Changed cam1 to cam2

--- src/org/usfirst/frc/team2783/robot/Robot.java
+++ src/org/usfirst/frc/team2783/robot/Robot.java
@@ -42,7 +42,7 @@ public class Robot extends IterativeRobot {
         usbCameraServer.setQuality(50);
         
         //the camera name (ex "cam0") can be found through the roborio web interface
-        usbCameraServer.startAutomaticCapture("cam1");
+        usbCameraServer.startAutomaticCapture("cam2");
         this.smartDashTable = NetworkTable.getTable("SmartDashboard");
         
         //Populate Autonomous chooser
