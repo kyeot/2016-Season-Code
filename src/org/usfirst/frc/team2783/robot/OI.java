6c9f25f4f7319a8f31859b415d1b086cfdb75171
Owenatkins29 <owenatkins39@yahoo.com>
2016-07-28 19:48:51 -0400

    Mapped motors and stuff.

--- src/org/usfirst/frc/team2783/robot/OI.java
+++ src/org/usfirst/frc/team2783/robot/OI.java
@@ -21,55 +21,55 @@ import edu.wpi.first.wpilibj.command.Command;
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
-public class OI {
+//public class OI {
 	
-	public static Joystick xBoxController = new Joystick(RobotMap.XBOX_CONTROLLER_ID);
-	public static Joystick manipulator = new Joystick(RobotMap.SHOOTER_JOYSTICK_ID);
+	//public static Joystick xBoxController = new Joystick(RobotMap.XBOX_CONTROLLER_ID);
+	//public static Joystick manipulator = new Joystick(RobotMap.SHOOTER_JOYSTICK_ID);
 
-	Trigger pivotLeftTrigger = new Dpad(xBoxController, 270);
-	Trigger pivotRightTrigger = new Dpad(xBoxController, 90);
-	Trigger moveForwardTrigger = new Dpad(xBoxController, 0);
-	Trigger moveDownwardTrigger = new Dpad(xBoxController, 180);
-	Button fineControlButton = new JoystickButton(xBoxController, 2);
+	//Trigger pivotLeftTrigger = new Dpad(xBoxController, 270);
+	//Trigger pivotRightTrigger = new Dpad(xBoxController, 90);
+	//Trigger moveForwardTrigger = new Dpad(xBoxController, 0);
+	//Trigger moveDownwardTrigger = new Dpad(xBoxController, 180);
+	//Button fineControlButton = new JoystickButton(xBoxController, 2);
 	
-	Trigger fullSpeedShooterTrigger = new Dpad(manipulator, 0);
-	Trigger cancelFullSpeedShooterTrigger = new Dpad(manipulator, 180);
-	Button reverseShooter = new JoystickButton(xBoxController, 5);
+	//Trigger fullSpeedShooterTrigger = new Dpad(manipulator, 0);
+	//Trigger cancelFullSpeedShooterTrigger = new Dpad(manipulator, 180);
+	//Button reverseShooter = new JoystickButton(xBoxController, 5);
 
-	Button adjusterToBottom = new JoystickButton(manipulator, 7);
-	Button adjusterToTop = new JoystickButton(manipulator, 8);
+	//Button adjusterToBottom = new JoystickButton(manipulator, 7);
+	//Button adjusterToTop = new JoystickButton(manipulator, 8);
 	
-	public Button ReverseBandsOnArmButton = new JoystickButton(manipulator, 7);
-	public Button retrieverInButton = new JoystickButton(manipulator, 5);
-	public Button retrieverOutButton = new JoystickButton(manipulator, 6);
-	public Button liftArm = new JoystickButton(manipulator, 4);
-	public Button lowerArm = new JoystickButton(manipulator, 3);
+	//public Button ReverseBandsOnArmButton = new JoystickButton(manipulator, 7);
+	//public Button retrieverInButton = new JoystickButton(manipulator, 5);
+	//public Button retrieverOutButton = new JoystickButton(manipulator, 6);
+	//public Button liftArm = new JoystickButton(manipulator, 4);
+	//public Button lowerArm = new JoystickButton(manipulator, 3);
 	
-	public OI() {
-		pivotLeftTrigger.whileActive(new PivotTankDrive());
-		pivotRightTrigger.whileActive(new PivotTankDrive());
+	//public OI() {
+		//pivotLeftTrigger.whileActive(new PivotTankDrive());
+		//pivotRightTrigger.whileActive(new PivotTankDrive());
 		
-		fineControlButton.toggleWhenPressed(new FineControl());
+		//fineControlButton.toggleWhenPressed(new FineControl());
 		
-		retrieverInButton.whenPressed(new ToggleRetriever(RetrieverDirection.RET_IN));
-		retrieverOutButton.whenPressed(new ToggleRetriever(RetrieverDirection.RET_OUT));
+		//retrieverInButton.whenPressed(new ToggleRetriever(RetrieverDirection.RET_IN));
+		//retrieverOutButton.whenPressed(new ToggleRetriever(RetrieverDirection.RET_OUT));
 		
-		liftArm.whenPressed(new SetArm(ArmDirection.ARM_UP));
-		liftArm.whenReleased(new SetArm(ArmDirection.ARM_STOP));
-		lowerArm.whenPressed(new SetArm(ArmDirection.ARM_DOWN));
-		lowerArm.whenReleased(new SetArm(ArmDirection.ARM_STOP));
+		//liftArm.whenPressed(new SetArm(ArmDirection.ARM_UP));
+		//liftArm.whenReleased(new SetArm(ArmDirection.ARM_STOP));
+		//lowerArm.whenPressed(new SetArm(ArmDirection.ARM_DOWN));
+		//lowerArm.whenReleased(new SetArm(ArmDirection.ARM_STOP));
 		
-		Command fullSpeedShooter = new ShooterAtAndControl(1.0);
-		fullSpeedShooterTrigger.whenActive(fullSpeedShooter);
-		cancelFullSpeedShooterTrigger.cancelWhenActive(fullSpeedShooter);
-		reverseShooter.whileHeld(new ShooterAtAndControl(-1.0));
+		//Command fullSpeedShooter = new ShooterAtAndControl(1.0);
+		//fullSpeedShooterTrigger.whenActive(fullSpeedShooter);
+		//cancelFullSpeedShooterTrigger.cancelWhenActive(fullSpeedShooter);
+		//reverseShooter.whileHeld(new ShooterAtAndControl(-1.0));
 
-		adjusterToTop.whenPressed(new AngleAdjusterToLimit(Limit.TOP_LIMIT));
-		adjusterToBottom.whenPressed(new AngleAdjusterToLimit(Limit.BOTTOM_LIMIT));
+		//adjusterToTop.whenPressed(new AngleAdjusterToLimit(Limit.TOP_LIMIT));
+		//adjusterToBottom.whenPressed(new AngleAdjusterToLimit(Limit.BOTTOM_LIMIT));
 		
 		//Gyro drive commands [untested]
 		//moveForwardTrigger.whileActive(new GyroCorrectedTankDrive());
 		//moveDownwardTrigger.whileActive(new GyroCorrectedTankDrive());
-	}
+	//}
 	
-}
+//}
--- src/org/usfirst/frc/team2783/robot/RobotMap.java
+++ src/org/usfirst/frc/team2783/robot/RobotMap.java
@@ -6,21 +6,33 @@ package org.usfirst.frc.team2783.robot;
  * floating around.
  */
 public class RobotMap {
-
+	
 	final public static int XBOX_CONTROLLER_ID = 0;
 	final public static int SHOOTER_JOYSTICK_ID = 1;
 	
-	final public static int FRONT_LEFT_MOTOR_ID = 16;
-	final public static int REAR_LEFT_MOTOR_ID = 1;
-	final public static int FRONT_RIGHT_MOTOR_ID = 15;
-	final public static int REAR_RIGHT_MOTOR_ID = 14;
+	final public static int FRONT_LEFT_1 = 0;
+	final public static int FRONT_LEFT_2 = 1;
+	final public static int FRONT_RIGHT_1 = 2;
+	final public static int FRONT_RIGHT_2 = 3;
+	final public static int REAR_LEFT_1 = 4;
+	final public static int REAR_LEFT_2 = 5;
+	final public static int REAR_RIGHT_1 = 6;
+	final public static int REAR_RIGHT_2 = 7;
+
+	//final public static int XBOX_CONTROLLER_ID = 0;
+	//final public static int SHOOTER_JOYSTICK_ID = 1;
 	
-	final public static int SHOOTER_WHEEL_MOTOR_ID = 13; 
-	final public static int SHOOTER_VERTICAL_AXIS_MOTOR_PWM_PORT = 4;
-	final public static int BALL_ELEVATOR_PWM_PORT = 2;
+	//public static int FRONT_LEFT_MOTOR_ID = 16;
+	//final public static int REAR_LEFT_MOTOR_ID = 1;
+	//final public static int FRONT_RIGHT_MOTOR_ID = 15;
+	//final public static int REAR_RIGHT_MOTOR_ID = 14;
 	
-	final public static int BALL_RETRIEVER_LEFT_MOTOR_PWM_PORT = 1;
-	final public static int BALL_RETRIEVER_RIGHT_MOTOR_PWM_PORT = 3;
-	final public static int RETRIEVER_ARM_MOTOR_PWM_PORT = 0;
+	//final public static int SHOOTER_WHEEL_MOTOR_ID = 13; 
+	//final public static int SHOOTER_VERTICAL_AXIS_MOTOR_PWM_PORT = 4;
+	//final public static int BALL_ELEVATOR_PWM_PORT = 2;
+	
+	//final public static int BALL_RETRIEVER_LEFT_MOTOR_PWM_PORT = 1;
+	//final public static int BALL_RETRIEVER_RIGHT_MOTOR_PWM_PORT = 3;
+	//final public static int RETRIEVER_ARM_MOTOR_PWM_PORT = 0;
 	
 }
