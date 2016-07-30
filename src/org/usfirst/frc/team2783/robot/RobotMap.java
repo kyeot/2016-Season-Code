273c5d67f153d7197c7e7d26dc650014cd0c87a2
Owenatkins29 <owenatkins39@yahoo.com>
2016-07-30 14:24:19 -0400

    Made the old RobotMap block-commented out.

--- src/org/usfirst/frc/team2783/robot/RobotMap.java
+++ src/org/usfirst/frc/team2783/robot/RobotMap.java
@@ -19,20 +19,21 @@ public class RobotMap {
 	final public static int REAR_RIGHT_WHEEL = 6;
 	final public static int REAR_RIGHT_SWIVEL = 7;
 
-	//final public static int XBOX_CONTROLLER_ID = 0;
-	//final public static int SHOOTER_JOYSTICK_ID = 1;
+	/**final public static int XBOX_CONTROLLER_ID = 0;
+	final public static int SHOOTER_JOYSTICK_ID = 1;
 	
-	//public static int FRONT_LEFT_MOTOR_ID = 16;
-	//final public static int REAR_LEFT_MOTOR_ID = 1;
-	//final public static int FRONT_RIGHT_MOTOR_ID = 15;
-	//final public static int REAR_RIGHT_MOTOR_ID = 14;
+	public static int FRONT_LEFT_MOTOR_ID = 16;
+	final public static int REAR_LEFT_MOTOR_ID = 1;
+	final public static int FRONT_RIGHT_MOTOR_ID = 15;
+	final public static int REAR_RIGHT_MOTOR_ID = 14;
 	
-	//final public static int SHOOTER_WHEEL_MOTOR_ID = 13; 
-	//final public static int SHOOTER_VERTICAL_AXIS_MOTOR_PWM_PORT = 4;
-	//final public static int BALL_ELEVATOR_PWM_PORT = 2;
+	final public static int SHOOTER_WHEEL_MOTOR_ID = 13; 
+	final public static int SHOOTER_VERTICAL_AXIS_MOTOR_PWM_PORT = 4;
+	final public static int BALL_ELEVATOR_PWM_PORT = 2;
 	
-	//final public static int BALL_RETRIEVER_LEFT_MOTOR_PWM_PORT = 1;
-	//final public static int BALL_RETRIEVER_RIGHT_MOTOR_PWM_PORT = 3;
-	//final public static int RETRIEVER_ARM_MOTOR_PWM_PORT = 0;
+	final public static int BALL_RETRIEVER_LEFT_MOTOR_PWM_PORT = 1;
+	final public static int BALL_RETRIEVER_RIGHT_MOTOR_PWM_PORT = 3;
+	final public static int RETRIEVER_ARM_MOTOR_PWM_PORT = 0;
+	*/
 	
 }
